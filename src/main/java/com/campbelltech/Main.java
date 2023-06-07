23fe01363f0441516ccba0102d55f67fd1657b89
sean <sean@campbelltech.co.za>
2023-06-07 22:33:59 +0200

    Fixed typo

--- src/main/java/com/campbelltech/Main.java
+++ src/main/java/com/campbelltech/Main.java
@@ -5,7 +5,7 @@ public class Main {
         HtmlPage htmlPage = new HtmlBuilder()
                 .AddTitle("Builder Pattern")
                 .AddHeader1("1. Definition:")
-                .AddParagraph("The builder pattern separate the construction of a complex object from its representation...")
+                .AddParagraph("The builder pattern separates the construction of a complex object from its representation...")
                 .AddHeader1("2. Class Diagram:")
                 .AddHeader2("2.1 Conceptual Diagram")
                 .AddParagraph("TODO: Add Conceptual Class Diagram here...")
